85c931c1fbc2a2ad38c481a535fab435074e1f41
Harikaran <harikaran7242@gmail.com>
2024-02-23 10:12:25 +0530

    added container

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -19,25 +19,27 @@ const Home = () => {
 
   return (
     <div>
-      <div className="row row-cols-1 row-cols-md-3 g-4 text-center">
-        {products.map((item, index) => (
-          <div key={index} className="col-md-4 mb-3">
-            <div className="card h-100">
-              <img
-                src={item.product_image}
-                className="card-img-top"
-                width={"100%"}
-                height={"350px"}
-                alt={item.product_name}
-              />
-              <div className="card-body">
-                <h5 className="card-title">{item.product_name}</h5>
-                <p className="card-text">{item.product_description}</p>
-                <p className="card-text">{item.product_price}</p>
+      <div className="container">
+        <div className="row row-cols-1 row-cols-md-3 g-4 text-center">
+          {products.map((item, index) => (
+            <div key={index} className="col-md-4 mb-3">
+              <div className="card h-100">
+                <img
+                  src={item.product_image}
+                  className="card-img-top"
+                  width={"100%"}
+                  height={"350px"}
+                  alt={item.product_name}
+                />
+                <div className="card-body">
+                  <h5 className="card-title">{item.product_name}</h5>
+                  <p className="card-text">{item.product_description}</p>
+                  <p className="card-text">{item.product_price}</p>
+                </div>
               </div>
             </div>
-          </div>
-        ))}
+          ))}
+        </div>
       </div>
     </div>
   );
